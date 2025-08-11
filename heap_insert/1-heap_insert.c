6a0ca243b7638f923a7f0a7ef4c80146e98fc3c6
1337rg <remi.garzoli@protonmail.com>
2025-08-11 17:05:45 +0200

    Fixing script for betty style code

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -2,46 +2,47 @@
 #include <stdlib.h>
 
 /**
- * swap_values - swap n values of two nodes
+ * swap_values - swaps n values of two heap nodes
  * @a: first node
  * @b: second node
  */
 static void swap_values(heap_t *a, heap_t *b)
 {
-    int tmp = a->n;
-    a->n = b->n;
-    b->n = tmp;
+	int tmp = a->n;
+
+	a->n = b->n;
+	b->n = tmp;
 }
 
 /**
- * find_parent - find first parent with missing child (BFS)
+ * find_parent - finds first parent with missing child using BFS
  * @root: root of the heap
- * Return: pointer to the parent node
+ * Return: pointer to the parent node, or NULL
  */
 static heap_t *find_parent(heap_t *root)
 {
-    heap_t **queue;
-    size_t front = 0, back = 0;
-    heap_t *node;
+	heap_t **queue;
+	size_t front = 0, back = 0;
+	heap_t *node;
 
-    queue = malloc(sizeof(*queue) * 1024);
-    if (!queue)
-        return (NULL);
+	queue = malloc(sizeof(*queue) * 1024);
+	if (!queue)
+		return (NULL);
 
-    queue[back++] = root;
-    while (front < back)
-    {
-        node = queue[front++];
-        if (!node->left || !node->right)
-        {
-            free(queue);
-            return (node);
-        }
-        queue[back++] = node->left;
-        queue[back++] = node->right;
-    }
-    free(queue);
-    return (NULL);
+	queue[back++] = root;
+	while (front < back)
+	{
+		node = queue[front++];
+		if (!node->left || !node->right)
+		{
+			free(queue);
+			return (node);
+		}
+		queue[back++] = node->left;
+		queue[back++] = node->right;
+	}
+	free(queue);
+	return (NULL);
 }
 
 /**
@@ -52,35 +53,34 @@ static heap_t *find_parent(heap_t *root)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *new, *parent;
+	heap_t *new, *parent;
 
-    if (!root)
-        return (NULL);
+	if (!root)
+		return (NULL);
 
-    if (*root == NULL)
-    {
-        *root = binary_tree_node(NULL, value);
-        return (*root);
-    }
+	if (*root == NULL)
+	{
+		*root = binary_tree_node(NULL, value);
+		return (*root);
+	}
 
-    parent = find_parent(*root);
-    if (!parent)
-        return (NULL);
+	parent = find_parent(*root);
+	if (!parent)
+		return (NULL);
 
-    new = binary_tree_node(parent, value);
-    if (!new)
-        return (NULL);
+	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
 
-    if (parent->left == NULL)
-        parent->left = new;
-    else
-        parent->right = new;
+	if (parent->left == NULL)
+		parent->left = new;
+	else
+		parent->right = new;
 
-    /* Bubble up */
-    while (new->parent && new->n > new->parent->n)
-    {
-        swap_values(new, new->parent);
-        new = new->parent;
-    }
-    return (new);
+	while (new->parent && new->n > new->parent->n)
+	{
+		swap_values(new, new->parent);
+		new = new->parent;
+	}
+	return (new);
 }
