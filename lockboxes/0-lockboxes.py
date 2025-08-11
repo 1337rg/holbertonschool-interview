349c573becc324dcc87fb37eaf2ddf55d521df2b
1337rg <remi.garzoli@protonmail.com>
2025-08-11 16:32:22 +0200

    fixed one error

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+Module for the lockboxes problem.
+Determines if all boxes can be unlocked starting from box 0.
+"""
+
 
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+        boxes (list of list of int): Each box contains keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
     if not isinstance(boxes, list) or len(boxes) == 0:
         return False
 
